7e27a79f66e5f02e8df950d07bcf43a46b0804c5
mazamine999 <101194544+mazamine999@users.noreply.github.com>
2023-02-15 02:32:57 +0100

    Update and rename Server.js to index.js

---
+++ BACK/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.send("<h1>Hello world</h1>");
+});
+
+app.listen(5000, () =>{
+    console.log("server started on port 5000");
+});
